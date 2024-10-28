ff845e756d0bc65a2d12ebaff3b6d54b2f7d3c28
unfunf22 <32785150+unfunf22@users.noreply.github.com>
2024-10-28 19:52:04 +0100

    updated for 2.0

--- prototypes/technologies.lua
+++ prototypes/technologies.lua
@@ -7,44 +7,37 @@ data:extend
     type = "technology",
     name = "logistic-system",
     icon = "__base__/graphics/technology/logistic-system.png",
-    icon_size=128,
-	effects =
-    
-	{
-      
-	{
-        
-		type = "unlock-recipe",
-
-        	recipe = "logistic-chest-active-provider"
-},
-
-
-	{
-       type = "unlock-recipe",
- 
-		recipe = "logistic-chest-buffer"
-},
-        {
-       
-		type = "unlock-recipe",
-
-	        recipe = "logistic-chest-requester"
-}    
-},
-    
-	prerequisites = { "construction-robotics", "logistic-robotics" },
+    icon_size = 256,
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "active-provider-chest"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "requester-chest"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "buffer-chest"
+      },
+      {
+        type = "vehicle-logistics",
+        modifier = true
+      }
+    },
+    prerequisites = {"logistic-robotics"},
     unit =
     {
-      count = 30,
+      count = 150,
       ingredients =
       {
-        {"automation-science-pack", 5},
-        {"logistic-science-pack", 5},
-	{"chemical-science-pack", 5}
-		
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1}
       },
-      time = 10
+      time = 30
     },
     order = "c-k-d",
   },
